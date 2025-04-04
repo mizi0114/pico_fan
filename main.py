38120e22180220bdc8d6e38a30e2eb50ea8ba57d
mizi0114 <130463435+mizi0114@users.noreply.github.com>
2025-04-04 09:26:53 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+from machine import Pin, PWM, ADC
+
+led = machine.Pin(14, Pin.OUT)
+led2 = machine.Pin(13)
+
+
+fan = machine.Pin(20)
+fan_pwm = PWM(fan)
+led_pwm = PWM(led)
+led_pwm2 = PWM(led2)
+duty_step = 129
+
+frequency = 5000
+fan_pwm.freq(frequency)
+led_pwm.freq(frequency)
+
+pot = ADC(Pin(26))
+led = machine.Pin(14, Pin.OUT)
+led2 = machine.Pin(13, Pin.OUT)
+led3 = machine.Pin(11, Pin.OUT)
+
+while True:
+    pot_value = pot.read_u16()
+    #led_pwm.duty_u16(pot_value)
+    fan_pwm.duty_u16(pot_value)
+    #print(pot_value)
+    if pot_value < 4000:
+        led.value(0)
+        led2.value(0)
+        led3.value(0)
+    elif pot_value < 50000:
+        led.value(1)
+        led2.value(0)
+        led3.value(0)
+    elif pot_value < 60000:
+        led.value(1)
+        led2.value(1)
+        led3.value(0)
+    elif pot_value > 60000:
+        led.value(1)
+        led2.value(1)
+        led3.value(1)
